dd9c26211879e688bfd9f0c5e3dee857f5d905d1
John Haugeland <stonecypher@gmail.com>
2020-12-16 22:59:23 -0800

    reformat peerJsEndpoint.ts

--- src/log.ts
+++ src/log.ts
@@ -1,12 +1,23 @@
+
 // TODO:
 // turn this into a structured log that:
 // - saves to in-memory log instead of `console.log` (slow)
 // - but can still be tailed somehow?
 
+
+
+
+
 const logEnabled = false;
 
-export const log = (...items: unknown[]): void => {
-  if (logEnabled) {
-    console.log(...items);
-  }
+function log(...items: unknown[]): void {
+  if (logEnabled) { console.log(...items); }
+};
+
+
+
+
+
+export {
+  log
 };
