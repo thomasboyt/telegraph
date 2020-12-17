cbd2012027d5cbbba23f4e0e87bd4732ce65b782
John Haugeland <stonecypher@gmail.com>
2020-12-17 01:37:52 -0800

    declare 0.1.0.  fix testing.  remove babel, eliminate all security threats, 2/3 of install size.  install ts-jest.  cleaner test regex.  remove unused js, tsx extension catches.  remove prettier.  fix removal of lodash.

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,23 @@
+
 module.exports = {
-  testEnvironment: "node",
-  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  coveragePathIgnorePatterns: ["/node_modules/", "/test/"],
-  coverageThreshold: {
-    global: {
-      branches: 90,
-      functions: 95,
-      lines: 95,
-      statements: 95,
+
+  testEnvironment            : 'node',
+  moduleFileExtensions       : ['js', 'ts'],
+  coveragePathIgnorePatterns : ["/node_modules/", "/test/"],
+  testMatch                  : ['**/*.test.ts', '**/*.spec.ts'],
+  transform                  : { '^.+\\.ts$': 'ts-jest' },
+  verbose                    : false,
+  collectCoverage            : true,
+
+  coverageThreshold : {
+    global : {
+      branches   : 90,
+      functions  : 95,
+      lines      : 95,
+      statements : 95,
     },
   },
-  collectCoverageFrom: ["src/*.{js,ts}"],
+
+  collectCoverageFrom: ["src/*.{js,ts}"]
+
 };
